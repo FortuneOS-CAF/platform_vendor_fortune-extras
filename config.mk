44b031bde05ee9f5bc07ad0552582e97549560d7
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2021-08-29 12:34:30 -0400

    fortune: overlay: Make Google Sans the default font
    Change-Id: I02b408de8b13a42b646aacb832a3bb7b620dd4c7

--- config.mk
+++ config.mk
@@ -13,3 +13,7 @@ PRODUCT_COPY_FILES += \
     vendor/fortune-extras/fonts/GoogleSans-Bold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Bold.ttf \
     vendor/fortune-extras/fonts/GoogleSans-BoldItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-BoldItalic.ttf \
     vendor/fortune-extras/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
+# Overlay
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/fortune-extras/overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/fortune-extras/overlay/common
