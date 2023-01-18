db64b69a9fb6ae24aaf4804861a3cc5ed84b3557
Sonal Singh <sonal.singh.19993@gmail.com>
2023-01-18 11:54:20 +0530

    fortune: Add custom themed icons  * Icons extracted from redfin beta 5
    Change-Id: I68185fb21c670d39e185426192dd329473070f01
    
    Launcher3: Import more themed icons
    
     * from pixel launcher mod
     * https://t.me/modulesrepo/3382
    
    Change-Id: Ifb10a802554243984c7b5eb71b4481b4e906b21b
    
    Launcher3: Specify hour index for clock themed icon
    
    - Setting it to null causes ClockDrawableWrapper to error
      while loading themed icon and reverts back to normal icon.
    
    Change-Id: I11d3c769054f444e723f7cb2223fe1e7c2507fa7

--- config.mk
+++ config.mk
@@ -29,5 +29,8 @@ PRODUCT_COPY_FILES += \
     vendor/fortune-extras/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
 
 # Overlay
+PRODUCT_PACKAGES += \
+    ThemedIconsOverlay
+
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/fortune-extras/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/fortune-extras/overlay/common
